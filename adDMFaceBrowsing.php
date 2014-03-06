982f600045f358fe936b768e100fa0179c517d7d
unknown <xhr@.(none)>
2014-03-06 20:28:33 +0800

    upload code

--- adDMFaceBrowsing.php
+++ adDMFaceBrowsing.php
@@ -25,10 +25,11 @@ try {
 	$tmp = array();
 	$personID = array();
 	$idx = 0;
+	$thres = 0.60;
 	for ($i = 0; $i < $count; $i++) {
 		// print_r($result[$i]);
 		// $tmp[$i] = $result[$i]["personID"];
-		if(in_array($result[$i]["personID"], $tmp))
+		if(in_array($result[$i]["personID"], $tmp)&&($result[$i]["score"]>=$thres))
 		{
 			$personID[$result[$i]["personID"]]=$personID[$result[$i]["personID"]]+1;
 			continue;
