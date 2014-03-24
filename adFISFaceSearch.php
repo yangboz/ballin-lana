f2a30043a5e44156f64379d3831ba00591dcee5b
unknown <xhr@.(none)>
2014-03-24 12:50:29 +0800

    upload code

--- adFISFaceSearch.php
+++ adFISFaceSearch.php
@@ -3,6 +3,11 @@
 	header("Access-Control-Allow-Origin: *");
 	
 	$os = (DIRECTORY_SEPARATOR=='\\')?"windows":'linux';
+	$page = intval($_POST['pageNum']);
+	// $page = 130;
+	$pageSize = 9; //每页显示数
+	$total = 0;
+	
 	$dir_path = '';
 	$res_file_path = '';
 	$win_lin = '';
@@ -11,9 +16,25 @@
 		case 'windows':
 			$res_file_path = 'data\\face_indexing_and_search_simres.txt';
 			$dir_path =  'data\\ExtractedFaces\\FacialImg\\Cluster_';
-			// echo $dir_path;
-			$sub_dir_path = 'data\\ExtractedFaces\\FacialImg\\Cluster_';
-			$win_lin = '\\';
+			$win_lin_suf = '\\';
+			$dir_num_len = 4;	//类文件的标号长度
+			$file = fopen($res_file_path, 'r');
+			$line = array();
+			$idx=0;
+			while (!feof($file)) {
+				$tmp = fgets($file);
+				$line[$idx++] = trim($tmp);
+			}
+			$res = array();
+			$total = $idx-1;	//类的个数
+			$res['total'] = $total;	
+			$res['pageSize'] = $pageSize;
+			$totalPage = ceil($total/$pageSize); //总页数
+			$res['totalPage'] = $totalPage;
+			$startPage = $page*$pageSize;
+			$res['list'] = get_data_win($dir_path,$line,$win_lin_suf,$startPage,$pageSize,$total,$dir_num_len);
+			fclose($file);
+			echo json_encode($res);
 			break;
 		
 		case 'linux':
@@ -23,62 +44,134 @@
 			// $dir_path = 'http://15.125.94.250/vica_web/data/ExtractedFaces/FacialImg/Cluster_';
 			$sub_dir_path = 'http://15.125.94.250/vica_web/data/ExtractedFaces/FacialImg/Cluster_';
 			
-			$win_lin = '/';
+			
+			$win_lin_suf = '/';
+			$dir_num_len = 4;	//类文件的标号长度
+			$file = fopen($res_file_path, 'r');
+			$line = array();
+			$idx=0;
+			while (!feof($file)) {
+				$tmp = fgets($file);
+				$line[$idx++] = trim($tmp);
+			}
+			$res = array();
+			$total = $idx-1;	//类的个数
+			$res['total'] = $total;	
+			$res['pageSize'] = $pageSize;
+			$totalPage = ceil($total/$pageSize); //总页数
+			$res['totalPage'] = $totalPage;
+			$startPage = $page*$pageSize;
+			$res['list'] = get_data_lin($dir_path,$sub_dir_path,$line,$win_lin_suf,$startPage,$pageSize,$total,$dir_num_len);
+			fclose($file);
+			echo json_encode($res);
 			break;
 		default:
 			
 			break;
 	}
 	
-	$file = fopen($res_file_path, 'r');
-	// echo $file;
-	$c = 0;
-	$dir_num_len = 4;
-	$idx=0;
-	$count=50;
-	$res = array();
-	$img_dir = '';
-	$sub_img_path = '';
-	$first_img_path = '';
-	while(!feof($file))
+	
+	function get_data_win($dir_path,$line,$win_lin_suf,$startPage,$pageSize,$total,$dir_num_len)
 	{
-		if($c==50) break;
-		$line = fgets($file);
-		$line = trim($line);
-		$res_array = explode("\t", $line);
-		arsort($res_array);
-		$dir_num = get_dir_num($idx, $dir_num_len);
-		$img_dir = $dir_path.$dir_num.$win_lin;
-		$sub_img_path = $sub_dir_path.$dir_num.$win_lin;
-		$first_img_path = get_dir_first_img_path($img_dir,$sub_img_path);
-		$res[$idx]['face_path'] = $first_img_path;
-		$res[$idx]['sim_count'] = count(glob($img_dir.'*.jpg'));
-		$data = array();
+		$result = array();
 		$index = 0;
-		foreach ($res_array as $key => $value) {
-			if($index == $count) break;
-			$dir_num = get_dir_num($key, $dir_num_len);
-			$img_dir = $dir_path.$dir_num.$win_lin;
-			$sub_img_path = $sub_dir_path.$dir_num.$win_lin;
-			$first_img_path = get_dir_first_img_path($img_dir,$sub_img_path);
-			// $first_img_path = get_dir_first_img_path($img_dir);
-			$img_count = count(glob($img_dir.'*.jpg'));
+		$count = 30;	//返回排在前$count=30个的相近类
+		$startIdx = $startPage;
+		$endIdx = $startIdx+$pageSize;
+		if($endIdx>$total) $endIdx=$total;
+		// echo $startIdx.'_'.$endIdx;
+		while ($startIdx<$endIdx) {
 			
-			$data[$index]['face_path'] = $first_img_path;
-			$data[$index]['img_dir'] = $img_dir;
-			// $data[$index]['origin_img_dir'] = $origin_dir_path;
-			// $data[$index]['sub_img_path'] = $sub_img_path;
-			$data[$index]['sim_count'] = $img_count;
-			$data[$index]['sim_value'] = $value;
+			$row = $line[$startIdx];
+			$res_array = explode("\t", $row);
+			arsort($res_array);
+			$dir_num = get_dir_num($startIdx, $dir_num_len);
+			$img_dir = $dir_path.$dir_num.$win_lin_suf;
+			$first_img_path = get_dir_first_img_path($img_dir,$img_dir);
+			$result[$index]['face_path'] = $first_img_path;
+			// $result[$index]['sim_count'] = count(glob($img_dir.'*.jpg'));
+			$data = array();
+			$idx = 0;
+			foreach ($res_array as $key => $value) {
+				if($idx == $count) break;
+				
+				$dir_num = get_dir_num($key, $dir_num_len);
+				$img_dir = $dir_path.$dir_num.$win_lin_suf;
+				$first_img_path = get_dir_first_img_path($img_dir,$img_dir);
+				// $first_img_path = get_dir_first_img_path($img_dir);
+				$img_count = count(glob($img_dir.'*.jpg'));
+				
+				$data[$idx]['face_path'] = $first_img_path;
+				$data[$idx]['img_dir'] = $img_dir;
+				// $data[$index]['origin_img_dir'] = $origin_dir_path;
+				// $data[$idx]['sub_img_path'] = $sub_img_path;
+				$data[$idx]['sim_count'] = $img_count;
+				$data[$idx]['sim_value'] = $value;
+				
+				$idx++;
+			}
+			
+			$result[$index]['data'] = $data;
 			$index++;
+			$startIdx++;
 		}
-		$res[$idx]['data'] = $data;
-		$idx++;
-		$c++;
+		
+		// print_r($result);
+		return $result;
 	}
-
-	fclose($file);
-	echo json_encode($res);
+	
+	
+	function get_data_lin($dir_path,$sub_dir_path,$line,$win_lin_suf,$startPage,$pageSize,$total,$dir_num_len)
+	{
+		$result = array();
+		$index = 0;
+		$count = 30;	//返回排在前$count=30个的相近类
+		$startIdx = $startPage;
+		$endIdx = $startIdx+$pageSize;
+		if($endIdx>$total) $endIdx=$total;
+		
+		while ($startIdx<$endIdx) {
+			
+			$row = $line[$startIdx];
+			$res_array = explode("\t", $row);
+			arsort($res_array);
+			$dir_num = get_dir_num($startIdx, $dir_num_len);
+			$img_dir = $dir_path.$dir_num.$win_lin_suf;
+			$sub_img_dir = $sub_dir_path.$dir_num.$win_lin_suf;
+			$first_img_path = get_dir_first_img_path($img_dir,$sub_img_dir);
+			$result[$index]['face_path'] = $first_img_path;
+			// $result[$index]['sim_count'] = count(glob($img_dir.'*.jpg'));
+			$data = array();
+			$idx = 0;
+			foreach ($res_array as $key => $value) {
+				if($idx == $count) break;
+				
+				$dir_num = get_dir_num($key, $dir_num_len);
+				$img_dir = $dir_path.$dir_num.$win_lin_suf;
+				$sub_img_dir = $sub_dir_path.$dir_num.$win_lin_suf;
+				$first_img_path = get_dir_first_img_path($img_dir,$sub_img_dir);
+				// $first_img_path = get_dir_first_img_path($img_dir);
+				$img_count = count(glob($img_dir.'*.jpg'));
+				
+				$data[$idx]['face_path'] = $first_img_path;
+				$data[$idx]['img_dir'] = $img_dir;
+				// $data[$index]['origin_img_dir'] = $origin_dir_path;
+				// $data[$idx]['sub_img_path'] = $sub_img_path;
+				$data[$idx]['sim_count'] = $img_count;
+				$data[$idx]['sim_value'] = $value;
+				
+				$idx++;
+			}
+			
+			$result[$index]['data'] = $data;
+			$index++;
+			$startIdx++;
+		}
+		
+		return $result;
+	}
+	
+	
 	
 	function get_dir_num($num,$len)
 	{
